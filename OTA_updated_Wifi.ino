63962cbeb012c7e8cdcc9a4629dcd1f87cc5c217
pauljayanta17 <60335458+pauljayanta17@users.noreply.github.com>
2021-07-05 18:53:10 +0530

    Add files via upload

---
+++ OTA_updated_Wifi.ino
@@ -0,0 +1,64 @@
+
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
+// Replace your own ssid and password (WiFi name and password)
+const char* ssid = "YOUR SSID";
+const char* password = "SSID PASSWORD";
+
+#define LED D5
+#define LED1 D8
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Booting");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Failed to connect ... Restarting");
+    delay(5000);
+    ESP.restart();
+  }
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  pinMode(LED, OUTPUT);
+  pinMode(LED1, OUTPUT);
+
+}
+
+void loop() {
+  ArduinoOTA.handle();
+ digitalWrite(LED, LOW);
+  delay(1000);
+  digitalWrite(LED, HIGH);
+  delay(1000);
+  digitalWrite(LED1, LOW);
+  delay(1000);
+  digitalWrite(LED1, HIGH);
+  delay(1000);
+}
+
+
+
+ 
